86f1e25027a7fdc7d5ad918fd3f18751d83a64b4
hunghg255 <giahunghust@gmail.com>
2024-10-16 22:07:58 +0700

    chore: update asset

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,7 +27,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel='shortcut icon' href='/favicon.ico' />
         <meta
           name='viewport'
-          content='width=device-width,initial-scale=1,maximum-scale1,shrink-to-fit=no'
+          content='width=device-width,initial-scale=1,maximum-scale=1,shrink-to-fit=no'
         />
 
         <meta
